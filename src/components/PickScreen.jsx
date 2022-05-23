301d6adcd70661812077418d71a8527719913a08
Andrey Larikov <larikov174@gmail.com>
2022-05-24 01:27:52 +0500

    feat: add svg

--- src/components/PickScreen.jsx
+++ src/components/PickScreen.jsx
@@ -27,18 +27,35 @@ const Image = styled.img`
     will-change: transform;
     transition: box-shadow 0.3s;
   }
-	&:active {
-		box-shadow: 0 0 20px #bbbbbb;
-	}
+  &:active {
+    box-shadow: 0 0 20px #bbbbbb;
+  }
 `;
 
 const PickScreen = () => {
   return (
-    <Container>
-      <Image src={rock} position="relative" top="-90px" left="-50%" />
-      <Image src={scissors} position="relative" top="-90px" left="0%" />
-      <Image src={paper} position="relative" top="130px" left="-360px" />
-    </Container>
+    <>
+      <Container>
+        <Image src={rock} position="relative" top="-90px" left="-50%" />
+        <Image src={scissors} position="relative" top="-90px" left="0%" />
+        <Image src={paper} position="relative" top="130px" left="-360px" />
+      </Container>
+      <svg>
+        <defs>
+          <pattern
+            id="imgpattern"
+            x="0"
+            y="0"
+            width="1"
+            height="1"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid slice">
+            <image width="100" height="100" href='https://i.pinimg.com/originals/e9/e0/b2/e9e0b2dc797104d5479796e99ee55fcd.jpg' />
+          </pattern>
+        </defs>
+        <circle cx={750} cy={50} r={100} fill="url(#imgpattern)" />
+      </svg>
+    </>
   );
 };
 
