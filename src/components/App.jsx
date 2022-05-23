d143b30e69fea2d3e61caefd5128f79aa9c26119
Andrey <larikov174@gmail.com>
2022-05-23 08:46:16 +0500

    fix: move styles out of component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import Header from './Header';
 
-const App = () => {
-  const MainPage = styled.div`
-    width: min(938px, 100%);
-		margin: 0 auto;
-  `;
+const MainPage = styled.div`
+  width: min(938px, 100%);
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const App = () => {
   return (
     <MainPage>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,55 @@
 import styled from 'styled-components';
 
-const Header = () => {
-  const MainContainer = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 32px;
-    margin: 48px auto;
-    border: 3px solid rgba(255, 255, 255, 0.28);
-    border-radius: 15px;
-    width: min(700px, 100%);
-    min-height: 150px;
-    @media screen and (max-width: 800px) {
-      /* margin: 32px; */
-      padding: 0 23px;
-    }
-    @media screen and (max-width: 375px) {
-      border-radius: 5px;
-    }
-  `;
-  const TitleContainer = styled.h1`
-    display: flex;
-    flex-direction: column;
-  `;
-  const MainTitle = styled.span`
-    font-weight: 700;
-    font-size: clamp(21px, 40px, 5vw);
-    line-height: clamp(16px, 32px, 1em);
-  `;
-  const ScoreContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(0deg, #f3f3f3 0%, #ffffff 100%);
-    box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.196706);
-    border-radius: 8px;
-    width: clamp(80px, 150px, 25vw);
-    height: clamp(80px, 115px, 25vw);
-  `;
-  const ScoreTitle = styled.p`
-    font-size: clamp(10px, 16px, 5vw);
-    line-height: clamp(12px, 19px, 0.5em);
-    color: #2a45c2;
-  `;
-  const ScoreResult = styled.p`
-    font-weight: 700;
-    font-size: clamp(40px, 64px, 18vw);
-    line-height: clamp(40px, 64px, 1.8em);
-    color: #565468;
-  `;
+const MainContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 32px;
+  margin: 48px auto;
+  border: 3px solid rgba(255, 255, 255, 0.28);
+  border-radius: 15px;
+  width: min(700px, calc(100% - 64px));
+  min-height: 150px;
+  @media screen and (max-width: 800px) {
+    padding: 0 23px;
+  }
+  @media screen and (max-width: 375px) {
+    border-radius: 5px;
+  }
+`;
+const TitleContainer = styled.h1`
+  display: flex;
+  flex-direction: column;
+`;
+const MainTitle = styled.span`
+  font-weight: 700;
+  font-size: clamp(21px, 40px, 5vw);
+  line-height: clamp(16px, 32px, 1em);
+`;
+const ScoreContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(0deg, #f3f3f3 0%, #ffffff 100%);
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.196706);
+  border-radius: 8px;
+  width: clamp(80px, 150px, 25vw);
+  height: clamp(80px, 115px, 25vw);
+`;
+const ScoreTitle = styled.p`
+  font-size: clamp(10px, 16px, 5vw);
+  line-height: clamp(12px, 19px, 0.5em);
+  color: #2a45c2;
+`;
+const ScoreResult = styled.p`
+  font-weight: 700;
+  font-size: clamp(40px, 64px, 18vw);
+  line-height: clamp(40px, 64px, 1.8em);
+  color: #565468;
+`;
 
+const Header = () => {
   return (
     <MainContainer>
       <TitleContainer>
